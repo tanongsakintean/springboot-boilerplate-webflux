8b78ba1f90026920780e6b5e1a7f8740b1b9e324
Tanongsak Intean <71477852+tanongsakintean@users.noreply.github.com>
2024-07-03 23:03:57 +0700

    first commit

---
+++ src/main/kotlin/com/springboot/boilerplate_webflux/BoilerplateWebfluxApplication.kt
@@ -0,0 +1,11 @@
+package com.springboot.boilerplate_webflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoilerplateWebfluxApplication
+
+fun main(args: Array<String>) {
+	runApplication<BoilerplateWebfluxApplication>(*args)
+}
